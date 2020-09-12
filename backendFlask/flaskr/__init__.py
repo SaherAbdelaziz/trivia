e61f66dda293d19837309d31afcced191448926f
saher sayed abdelaziz <SaherSayed@cis.asu.edu.eg>
2020-09-12 18:08:52 +0200

    implement create new q delete q search q per c

--- backendFlask/flaskr/__init__.py
+++ backendFlask/flaskr/__init__.py
@@ -43,7 +43,7 @@ def create_app(test_config=None):
     done
     '''
     @app.route('/categories', methods=['GET'])
-    def get_categories():
+    def GetCategories():
         """
         This endpoint returns all categories or status code 500 if there is a server error
         """
@@ -72,7 +72,7 @@ def create_app(test_config=None):
     done
     '''
     @app.route('/questions', methods=['GET'])
-    def get_questions():
+    def GetQuestions():
         """
         an endpoint to handle GET requests for questions, 
         including pagination (every 10 questions).  and returns a 404
@@ -100,48 +100,147 @@ def create_app(test_config=None):
 
         # return values if there are no errors
         return jsonify({
-            'success': aTrue,
+            'success': True,
             'total_questions': total_questions,
             'categories': categories,
             'questions': current_questions
         }), 200
     '''
 
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
+    TEST: At this point, when you start the application
+    you should see questions and categories generated,
+    ten questions per page and pagination at the bottom of the screen for three pages.
+    Clicking on the page numbers should update the questions. 
+    '''
 
     '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+    @TODO: 
+    Create an endpoint to DELETE question using a question ID. 
+    done
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+    TEST: When you click the trash icon next to a question, the question will be removed.
+    This removal will persist in the database and when you refresh the page. 
+    '''
+    @app.route('/questions/<int:id>', methods=['DELETE'])
+    def DeleteQuestion(id):
+        '''
+        An endpoint to DELETE question using an ID.
+        if any erorr expet send 422
+        '''
+        try:
+            question = Question.query.get(id)
+            question.delete()
+
+            return jsonify({
+                'success': True,
+                'message': "Question successfully deleted",
+                'delete_id': id
+            })
+        except Exception:
+            abort(422)
 
     '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
+    @TODO: 
+    Create an endpoint to POST a new question, 
+    which will require the question and answer text, 
+    category, and difficulty score.
+    '''
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
+    @app.route('/questions/new', methods=['POST'])
+    def NewQuestion():
+        '''
+        An endpoint to POST a new question,retrurn 422 if data is not completed 
+        '''
+        # get data
+        question = request.get_json().get('question', '')
+        answer = request.get_json().get('answer', '')
+        difficulty = request.get_json().get('difficulty', '')
+        category = request.get_json().get('category', '')
+
+        # validation
+        if ((question == '') or (answer == '')
+                or (difficulty == '') or (category == '')):
+            abort(422)
+
+        try:
+            # Create a new question
+            question = Question(
+                question=question,
+                answer=answer,
+                difficulty=difficulty,
+                category=category)
+
+            # save question
+            question.insert()
+
+            # return success message
+            return jsonify({
+                'success': True,
+                'message': 'Question successfully created!'
+            }), 201  # ok created
+
+        except Exception:
+            abort(422)
 
     '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
+    TEST: When you submit a question on the "Add" tab, 
+    the form will clear and the question will appear at the end of the last page
+    of the questions list in the "List" tab.  
+    '''
+
+    '''
+    @TODO: 
+    Create a POST endpoint to get questions based on a search term. 
+    It should return any questions for whom the search term 
+    is a substring of the question. 
+    '''
+    @app.route('/questions/search', methods=['POST'])
+    def search_questions():
+        '''
+        Create a POST endpoint to get questions based on a search term 
+        '''
+
+        search_term = request.get_json().get('searchTerm', '')
+
+        # Return 422 status code if empty search term is sent
+        if search_term == '':
+            abort(422)
+
+        try:
+            # get all questions that has the search term substring
+            questions = Question.query.filter(
+                Question.question.ilike(f'%{search_term}%')).all()
+
+            # if there are no questions for search term return 404
+            if len(questions) == 0:
+                abort(404)
+
+            page = request.args.get('page', 1, type=int)
+            start = (page - 1) * nOfQuestions
+            end = start + nOfQuestions
+
+            questions = [question.format() for question in questions]
+            current_questions = questions[start:end]
+
+            # return response if successful
+            return jsonify({
+                'success': True,
+                'questions': current_questions,
+                'total_questions': len(Question.query.all())
+            }), 200
+
+        except Exception:
+            # This error code is returned when 404 abort
+            # raises exception from try block
+            abort(404)
+
+    '''
+
+    TEST: Search by any phrase. The questions list will update to include 
+    only question that include that string within their question. 
+    Try using the word "title" to start. 
+    '''
 
     '''
   @TODO: 
